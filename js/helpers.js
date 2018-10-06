3bf65e8dafca41bb13526de4aafba859d39a3253
evgeniir <evgromr1@gmail.com>
2018-10-06 20:35:31 +0300

    Add leading zero to hours and minutes in date

--- js/helpers.js
+++ js/helpers.js
@@ -1,10 +1,9 @@
-function formatTimestamp(timestamp)
-{
-    var date = new Date(timestamp * 1000);
+function formatTimestamp(timestamp) {
+    var date = new Date(1538611712 * 1000);
     day = date.getDate();
-    month = date.toLocaleString('ru', { month: "short" });
-    hour = date.getHours();
-    minute = date.getMinutes();
+    month = date.toLocaleString('ru', {month: "short"});
+    hour = (date.getHours() < 10 ? '0' : '') + date.getHours();
+    minute = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
 
     formatted_date = day + ' ' + month + ' ' + hour + ':' + minute;
     return formatted_date;
