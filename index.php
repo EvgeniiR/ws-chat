eb46fd0747117df5b2c2a82b8f1b7bd9de48c58a
evgeniir <evgromr1@gmail.com>
2018-10-07 09:08:18 +0300

    Mobile compatibility update

--- index.php
+++ index.php
@@ -3,13 +3,10 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="css/style.css">
-<!--<script src="./js/jquery.js"></script>-->
-<div class="container">
-    <div class="row">
-        <div class="col-lg-6 col-md-10 center-block" id="chatbox"></div>
-    </div>
-    <textarea class="col-lg-6 col-md-10 center-block" id="textbox"></textarea>
-    <button class="col-lg-6 col-md-10 center-block btn-send" onclick="send()">Отправить</button>
+<div class="container-fluid">
+    <div class="col-xl-8 center-block" id="chatbox"></div>
+    <textarea class="col-xl-8 center-block" id="textbox"></textarea>
+    <button class="col-xl-8 center-block btn-send" onclick="send()">Отправить</button>
 </div>
 <script src="./js/helpers.js"></script>
 <script src="./js/script.js"></script>
