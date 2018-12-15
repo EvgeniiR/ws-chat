decf27a9926762d6a7b8eacce6814f28a47c78c7
evgeniir <evgromr1@gmail.com>
2018-12-15 21:15:53 +0300

    Fix word wrap2.

--- resources/js/components/MessagesView.vue
+++ resources/js/components/MessagesView.vue
@@ -2,8 +2,7 @@
     <div>
         <div class="w-100" id="chatbox">
             <div v-for="(message) in messages">
-                <span class="d-inline" v-html="formatTimestamp(message.dateTime) + ':' + message.username+ '. '"></span>
-                <span class="d-inline" v-html="formatMessage(message.message)"></span>
+                <span v-html="formatTimestamp(message.dateTime) + ':' + message.username+ '. ' + formatMessage(message.message)"></span>
                 <br>
             </div>
         </div>
